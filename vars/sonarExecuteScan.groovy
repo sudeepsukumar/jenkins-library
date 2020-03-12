e6af60b8ceaadbfbcbe5c2d75666bcf4f3fa20f7
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2020-03-12 11:11:49 +0100

    Update sonarExecuteScan.groovy

--- vars/sonarExecuteScan.groovy
+++ vars/sonarExecuteScan.groovy
@@ -139,9 +139,12 @@ void call(Map parameters = [:]) {
                         if(config.projectVersion) config.options.add("sonar.projectVersion=${config.projectVersion}")
                         // prefix options
                         config.options = config.options.collect { it.startsWith('-D') ? it : "-D${it}" }
-
-                        sh "printenv"
-                        sh "PATH=\$PATH:'${env.WORKSPACE}/.sonar-scanner/bin' sonar-scanner -X ${config.options.join(' ')}"
+                        withEnv([
+                            "SONAR_SCANNER_OPTS='-Djavax.net.debug=all -Djavax.net.ssl.trustStore=${env.WORKSPACE}/.certificates/cacerts -Djavax.net.ssl.trustStorePassword=changeit'"
+                        ]){
+                            sh "printenv"
+                            sh "PATH=\$PATH:'${env.WORKSPACE}/.sonar-scanner/bin' sonar-scanner -X ${config.options.join(' ')}"
+                        }
                 }
             } finally {
                 //sh 'rm -rf .sonar-scanner .certificates .scannerwork'
