8e1f921eccb919b65bfaa6d52019e70985b6772e
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2020-03-12 11:07:11 +0100

    Update sonarExecuteScan.groovy

--- vars/sonarExecuteScan.groovy
+++ vars/sonarExecuteScan.groovy
@@ -128,9 +128,9 @@ void call(Map parameters = [:]) {
                         loadSonarScanner(config)
 
                         if(fileExists('.certificates/cacerts')){
-                            config.options.add("javax.net.ssl.trustStore=${env.WORKSPACE}/.certificates/cacerts")
-                            config.options.add("javax.net.ssl.trustStorePassword=changeit")
-                            //sh "export JAVA_OPTIONS='-Djavax.net.ssl.trustStore=${env.WORKSPACE}/.certificates/cacerts -Djavax.net.ssl.trustStorePassword=changeit'"
+                            //config.options.add("javax.net.ssl.trustStore=${env.WORKSPACE}/.certificates/cacerts")
+                            //config.options.add("javax.net.ssl.trustStorePassword=changeit")
+                            sh "export JAVA_OPTIONS='-Djavax.net.debug=all -Djavax.net.ssl.trustStore=${env.WORKSPACE}/.certificates/cacerts -Djavax.net.ssl.trustStorePassword=changeit'"
                             //sh 'mv .certificates/cacerts .sonar-scanner/jre/lib/security/cacerts'
                         }
 
